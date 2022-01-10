777281280ef1716adcdb48aa21147432d7226cfa
cjgochanour <cjgochanour@gmail.com>
2022-01-10 15:59:31 -0600

    render employee specialties in DOM

--- src/components/customers/CustomerList.js
+++ src/components/customers/CustomerList.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 
 export const CustomerList = () => {
     const [customers, setCustomers] = useState([]);
+    const [totalCustomers, updateMsg] = useState("");
 
     useEffect(() => {
         fetch("http://localhost:8088/customers")
@@ -11,11 +12,20 @@ export const CustomerList = () => {
             });
     }, []);
 
+    useEffect(() => {
+        if (customers.length === 1) {
+            updateMsg("You have 1 customer");
+        } else {
+            updateMsg(`You have ${customers.length} customers`);
+        }
+    }, [customers]);
+
     return (
         <>
             <h2>Customer List</h2>
+            <div>{totalCustomers}</div>
             <ul>
-                {customers.map((customer) => (
+                {customers.slice(0, 5).map((customer) => (
                     <li key={`customer--${customer.id}`}>{customer.name}</li>
                 ))}
             </ul>
--- src/components/employees/EmployeeList.js
+++ src/components/employees/EmployeeList.js
@@ -1,24 +1,29 @@
 import React, { useEffect, useState } from "react";
 
 export const EmployeeList = () => {
-    const [employees, setEmployees] = useState([]);
+    const [employees, changeEmployee] = useState([]);
+    const [specialties, setSpecialty] = useState("");
 
     useEffect(() => {
         fetch("http://localhost:8088/employees")
             .then((res) => res.json())
-            .then((employeeArray) => {
-                setEmployees(employeeArray);
+            .then((data) => {
+                changeEmployee(data);
             });
     }, []);
+
+    useEffect(() => {
+        const x = employees.map((employee) => employee.specialty);
+        setSpecialty(x.join(", "));
+    }, [employees]);
 
     return (
         <>
             <h2>Employee List</h2>
-            <ul>
-                {employees.map((employee) => (
-                    <li key={`employee--${employee.id}`}>{employee.name}</li>
-                ))}
-            </ul>
+            <div>Specialties: {specialties}</div>
+            {employees.map((employee) => {
+                return <p key={`employee--${employee.id}`}>{employee.name}</p>;
+            })}
         </>
     );
 };
