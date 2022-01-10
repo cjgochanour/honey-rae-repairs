cc14f102ce5a9db45990126cecb2c89ef764faa8
cjgochanour <cjgochanour@gmail.com>
2022-01-10 13:40:34 -0600

    modularized by creating customerlist component

--- src/components/Repairs.js
+++ src/components/Repairs.js
@@ -1,26 +1,10 @@
-import React, { useEffect, useState } from "react";
-
-const database = {
-    customers: [],
-};
+import { CustomerList } from "./customers/CustomerList.js";
 
 export const Repairs = () => {
-    const [customers, setCustomers] = useState([]);
-
-    useEffect(() => {
-        fetch("http://localhost:8088/customers")
-            .then((res) => res.json())
-            .then((customerArray) => {
-                setCustomers(customerArray);
-            });
-    }, []);
-
     return (
         <>
             <h1>Honey Rae's Repair Shop</h1>
-            {customers.map((customer) => (
-                <h2>{customer.name}</h2>
-            ))}
+            <CustomerList />
         </>
     );
 };
